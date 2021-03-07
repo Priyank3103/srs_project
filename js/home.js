bfceb77aff52074598fa026479f9a6966bac8abe
RanpuraDipen <dipencranpura@gmail.com>
2021-03-07 09:45:35 +0530

    Add it more

---
+++ js/home.js
@@ -0,0 +1,39 @@
+$(function (){
+        
+
+    // show/hide nav on page load
+    showHideNav();
+    
+    $(window).scroll(function(){
+        
+        // // show/hide nav on window's scroll
+        showHideNav();
+    })
+    
+    function showHideNav(){
+        
+        if( $(window).scrollTop() > 50 ){
+            
+            // Show White nav
+            $('.wrapper').addClass("white-header");
+            
+            // Show Dark nav
+            $(".logo-wrapper img").attr("src", "img/home/logo.png");
+            
+            // Show Back To Top Button
+//            $("#back-to-top").fadeIn();
+            
+        } else {
+            
+            // Hide White nav
+            $('.wrapper').removeClass("white-header");
+            
+            // Show logo
+            $(".logo-wrapper img").attr("src", "img/home/top-logo.png");
+            
+            // Hide back to top button
+//            $("#back-to-top").fadeOut();
+        }
+        
+    }
+});
